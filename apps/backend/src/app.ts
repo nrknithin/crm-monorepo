912cebb9ee153e321cfaa403191e6fdc509d822c
Nithin R Krishnan <nithin.r.krishnan@ibm.com>
2025-05-15 19:56:46 +0530

    init

---
+++ apps/backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app = express();
+
+// Middleware setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
